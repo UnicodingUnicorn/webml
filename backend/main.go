1c84a218933f8fae0dcf7ad41d9de6720f95bfdb
UnicodingUnicorn <7555ic@gmail.com>
2019-11-23 00:42:16 +0800

    Additional CORS headers

--- backend/main.go
+++ backend/main.go
@@ -97,6 +97,8 @@ func main() {
 func AddCors(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
 		handler.ServeHTTP(w, r)
 	})
 }
